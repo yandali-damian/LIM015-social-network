5769a0b1d90815a1ec7228e5c24da9fc97d0cc39
irwinet <ydamianelguera@gmail.com>
2021-09-20 13:01:08 -0500

    se agrego valicaciones a los imput de register

--- src/views/signup.js
+++ src/views/signup.js
@@ -12,9 +12,14 @@ const signup = () => {
                                 <span class="span-signup">&boxh;&boxh;&boxh;&boxh;&boxh; Regístrate &boxh;&boxh;&boxh;&boxh;&boxh;</span>
                                 <form action="#" class="form-signup" id="formSignup">
                                     <input type="text" class="name-signup" id="nameSignup" placeholder="Name" />
+                                    <span id="pName" class="span-name">* Ingrese su nombre</span>
                                     <input type="email" class="email-signup" id="emailSignup" placeholder="Email" />
+                                    <span id="pEmail" class="span-email">* Ingrese su Email</span>
                                     <input type="password" class="password-signup" id="passwordSignup" placeholder="Password" />
+                                    <span id="pPasword" class="span-password">* Ingrese su contraseña</span>
                                     <input type="password" class="confirm-signup" id="confirmSignup" placeholder="Confirm Password" />
+                                    <span id="pConfPasword" class="span-confpassword">* Confirme su contraseña</span>
+                                    <span id="smsEP"></span>
                                     <button type="submit" class="btn-signup" id="btnSignup">Registrarse</button>
                                 </form>
                                 <span class="span-loginWith">O ingresa con ...</span>
