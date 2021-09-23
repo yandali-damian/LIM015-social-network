24f7b58fe96bbf0621026ff1ec7f250a4e0571e4
Grecia Llatas <greciastefanni96@gmail.com>
2021-09-23 16:27:14 -0500

    Barra de navegacion completa

--- src/view-controler/path-index.js
+++ src/view-controler/path-index.js
@@ -9,23 +9,33 @@ export const changeView = (route) => {
     const container = document.getElementById('container');
     container.innerHTML = ""; //Limpia en contenedor de las vistas
 
+    const header = document.querySelector('#headerHome');
+    const footer = document.querySelector('#footer');
+
     switch (route) {
         case '':
         case '#':
         case '#/':
             {
+                header.style.display = 'none';
+                footer.style.display = 'none';
                 const login = container.appendChild(components.login());
                 loginDB();
                 return login
             }
         case '#/signup':
             {
+                header.style.display = 'none';
+                footer.style.display = "none";
                 const signup = container.appendChild(components.signup());
                 createAccount();
                 return signup;
             }
         case '#/home':
-            { return container.appendChild(components.home()) }
+            {
+                const home = container.appendChild(components.home());
+                return home
+            }
         default:
             { return container.appendChild(components.error()) }
     }
